ad1efef85e6bd5b581575a35ad4d4e312d6f81ee
Andrew Shih <andrewtshih@berkeley.edu>
2024-11-14 20:44:26 -0500

    began inserting values into tables

--- load-ipeds.py
+++ load-ipeds.py
@@ -5,15 +5,13 @@ import credentials_proj
 
 filename = sys.argv[1]
 
-# load csv as pandas df
+# load the actual dataset passed in as a command argument as a csv
 ipeds_df = pd.read_csv(filename, encoding='unicode_escape')
-ipeds_df['ZIP'] = ipeds_df['ZIP'].str.slice(0, 5)
+# load the ipeds data dictionary
+ipeds_dict_df = pd.read_excel('ipeds_dict.xlsx', sheet_name="Frequencies")[["varname", "codevalue", "valuelabel"]]
 
-# preprocessing
-
-# conversions
-cities = ipeds_df[['CITY', 'STABBR']].drop_duplicates()
-
+# # conversions
+# zips = ipeds_df['ZIP'].str.slice(0, 5)
 
 conn = psycopg.connect(
     host="pinniped.postgres.database.azure.com",
@@ -28,55 +26,99 @@ num_rows_inserted = 0
 
 # make a new transaction
 with conn.transaction():
-    # for i, row in cities.iterrows():
-    try:
-        #         # make a new SAVEPOINT -- like a save in a video game
-        #         with conn.transaction():
-        # perhaps a bunch of reformatting and data manipulation goes here
 
-        # now insert the data
-        # cur.execute("""
-        #             INSERT INTO institutions_static (unit_id,
-        #                 agency_id,
-        #                 preddeg_id,
-        #                 highdeg_id,
-        #                 control_id,
-        #                 region_id,
-        #                 instnm,
-        #                 addr,
-        #                 city_id,
-        #                 zip,
-        #                 c21basic_id,
-        #                 c21ipug_id,
-        #                 c21ipgrd_id,
-        #                 c21ugprf_id,
-        #                 c21enprf_id,
-        #                 c21szset_id,
-        #                 cbsa_id,
-        #                 county_cd,
-        #                 latitude,
-        #                 longitude)
-        #             VALUES
-        #             """,
-        #             (row[]))
-        cur.executemany("""
-                    INSERT INTO cities (city_id, city, stabbr)
-                    VALUES (%s, %s, %s)
-                    """,
-                        [(i + 1, row['CITY'], row['STABBR']) for i, row in cities.iterrows()])
-    except Exception as e:
-        # if an exception/error happens in this block, Postgres goes back
-        # to
-        # the last savepoint upon exiting the `with` block
-        print("insert failed:" + str(e))
-        # add additional logging, error handling here
+    # controls
+    cur.execute("SELECT COUNT(*) FROM controls")
+    if cur.fetchone()[0] == 0:
+        try:
+            controls = ipeds_dict_df[ipeds_dict_df['varname'] == 'CONTROL']
+            cur.executemany("""
+                            INSERT INTO controls (control_id, control)
+                            VALUES (%s, %s)
+                            """,
+                            [(row['codevalue'],  None) if row['codevalue'] == "-3" else
+                                (row['codevalue'], row['valuelabel']) for i, row in controls.iterrows()])
+        except psycopg.errors.UniqueViolation as e:
+            print("Attempted to insert a duplicate key value:", e)
+            conn.rollback()
     else:
-        # no exception happened, so we continue without reverting the
-        # savepoint
-        num_rows_inserted += 1
+        print("Data already loaded in controls table.")
+
+    # regions
+    cur.execute("SELECT COUNT(*) FROM regions")
+    if cur.fetchone()[0] == 0:
+        try:
+            regions = ipeds_dict_df[ipeds_dict_df['varname'] == 'OBERREG']
+            cur.executemany("""
+                            INSERT INTO regions (region_id, region)
+                            VALUES (%s, %s)
+                            """,
+                            [(row['codevalue'], row['valuelabel']) for i, row in regions.iterrows()])
+        except psycopg.errors.UniqueViolation as e:
+            print("Attempted to insert a duplicate key value:", e)
+            conn.rollback()
+    else:
+        print("Data already loaded in regions table.")
+
+    # ccs_basic
+    cur.execute("SELECT COUNT(*) FROM ccs_basic")
+    if cur.fetchone()[0] == 0:
+        try:
+            ccs_basic = ipeds_dict_df[ipeds_dict_df['varname'] == 'C21BASIC']
+            cur.executemany("""
+                            INSERT INTO ccs_basic (cc_basic_id, cc_basic)
+                            VALUES (%s, %s)
+                            """,
+                            [(row['codevalue'],  None) if row['codevalue'] == "-2" else
+                                (row['codevalue'], row['valuelabel']) for i, row in ccs_basic.iterrows()])
+        except psycopg.errors.UniqueViolation as e:
+            print("Attempted to insert a duplicate key value:", e)
+            conn.rollback()
+    else:
+        print("Data already loaded in ccs_basic table.")
+
+    # institutions_static
+    # cur.execute("SELECT COUNT(*) FROM institutions_static")
+    # if cur.fetchone()[0] == 0:
+    #     try:
+    #         cur.executemany("""
+    #                         INSERT INTO institutions_static (
+    #                         unit_id, instnm, addr, city_id, zip,
+    #                         county_cd, cbsa_id, region_id, latitude, longitude, control_id,
+    #                         c21basic_id, c21ipug_id, c21ipgrd_id,
+    #                         c21ugprf_id, c21enprf_id, c21szset_id)
+    #                         VALUES (%s, %s, %s, %s, %s,
+    #                         %s, %s, %s, %s, %s, %s,
+    #                         %s, %s, %s,
+    #                         %s, %s, %s)
+    #                     """,
+    #                     [(row['UNITID'], row['INSTNM'], row['ADDR'], ) for i, row in ipeds_df.iterrows()])
+    #     except Exception as e:
+    #         print()
+
+    # try:
+    #     cities = ipeds_df[['CITY', 'STABBR']].drop_duplicates()
+    #     cur.executemany("""
+    #                     INSERT INTO cities (city_id, city, stabbr)
+    #                     VALUES (%s, %s, %s)
+    #                     """,
+    #                     [(i + 1, row['CITY'], row['STABBR']) for i, row in cities.iterrows()])
+    # except Exception as e:
+    #     # if an exception/error happens in this block, Postgres goes back
+    #     # to
+    #     # the last savepoint upon exiting the `with` block
+    #     print("insert failed:" + str(e))
+    #     # add additional logging, error handling here
+    #     conn.rollback()
+
+    # else:
+    #     # no exception happened, so we continue without reverting the
+    #     # savepoint
+    #     num_rows_inserted += len(ipeds_df)
 
 # now we commit the entire transaction
 conn.commit()
+print("Number of rows inserted into tables", num_rows_inserted)
 
 # try:
 #     cur.execute("""
